ee09228400b5a3e881a6da7c2b1db99c73ec995e
faisalanwar11 <faisal.anwar1999@gmail.com>
2025-06-12 18:41:55 +0000

    Add output for web server public URL

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,7 @@
 # Displays the public IP address of the EC2 instance in URL format (http://...)
 # ------------------------------------------------------------------------------
 
+
 output "webserver_url" {
   value       = "http://${aws_instance.webserver.public_ip}"
   description = "The public IP address of the web server"
